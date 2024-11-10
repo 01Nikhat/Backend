22de38eaa8379dbdd59ad8b9dfadb2eba6d9d27f
Nikhat Parvin <nikhat.parvin013@gmail.com>
2024-11-10 19:52:57 +0530

    second commit

--- practice-10-airbnb/app.js
+++ practice-10-airbnb/app.js
@@ -1,52 +1,43 @@
 
 const express = require('express');//external module
-const bodyParser = require('body-parser');
-
+const userRouter = require("./routes/userRouter");//local module
 
 const app = express();
 
 
 
-app.use('/',(req,res,next) =>{
-  console.log('came in first middleware',req.url,req.method);
-   next();
-});
-
-app.use('/',(req,res,next) =>{
-  console.log('came in second middleware',req.url,req.method);
+app.get((req,res,next) =>{
+  console.log(req.url,req.method);
   next();
 });
-app.get('/',(req,res,next) =>{
-  console.log('came in third middleware',req.url,req.method);
-  res.send("<p>Welcome to third middleware</p>"); 
-});
 
-app.get('/contact-us',(req,res,next) =>{
+
+app.use(express.urlencoded());
+
+app.use(userRouter);
+
+app.get('/host/add-home',(req,res,next) =>{
   res.send(
-  `<html>
-    <head><title>Practice set</title></head>
-    <body>
-    <h1>Enter Your Details</h1>
-    <form action="/contact-us" method="POST">
-    <input type="text" name="username" placeholder="Enter Your Name" />
-    <input type="text" name="email" placeholder="Enter Your Email" />
-    <input type="submit" value="Submit" />
-    </form>
-    </body>
-  </html>`); 
+  `<h1>Register Your home here</h1>
+  <form action="/host/add-home" method="POST">
+      <input type="text" name="houseName" placeholder="Enter Your Name of your house"/>
+      <input type="submit" />
+      
+  </form>`
+  ); 
+  
 });
 
-app.post('/contact-us',(req,res,next) =>{
-  console.log('handling / constct-us for post',req.url,req.method);
-  res.send("<p>checking body-parser method</p>"); 
+app.post('/host/add-home',(req,res,next) =>{
+  console.log(req.body);
+  
+  res.send(
+  `<h1>Register Successfully</h1>
+  <a href="/">Go to home page</a>`
+  ); 
+  
 });
 
-app.use(bodyParser,bodyParser.urlencoded());
-
-app.post('/contact-us',(req,res,next) =>{
-  console.log('constct-us for post',req.url,req.method);
-  res.send("<p>Shortly Meet You Sonn</p>"); 
-});
 
 //const server = http.createServer(app);
 
